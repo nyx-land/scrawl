bbac757cb8fbe8691852bd35ed3e7748cd292dc7
Nyx <n1x@riseup.net>
2025-05-01 23:46:48 -0700

    partially working

--- src/parser.lisp
+++ src/parser.lisp
@@ -16,7 +16,8 @@
    :<*>
    :between
    :parse
-   :take-while)
+   :take-while
+   :consume)
   (:import-from
    :common-doc
    :reference
@@ -36,7 +37,24 @@
    :section
    :document
    :paragraph
-   :children))
+   :children
+   :bold
+   :italic
+   :underline
+   :strikethrough
+   :code
+   :code-block
+   :web-link
+   ;; constructors
+   :make-bold
+   :make-italic
+   :make-underline
+   :make-strikethrough
+   :make-code
+   :make-code-block
+   :make-text
+   :make-web-link
+   :make-section))
 
 (in-package :scrawl.parser)
 
@@ -52,57 +70,101 @@
 
 ;;;; utils ------------------------------------------------------------
 
-(defun whitespace-p (c)
-  (member c *whitespace*))
-
 (defun sexp-end (c)
   (char= c +sexp-close+))
 
 (defun sexp-start (c)
   (char= c +sexp-open+))
 
+(defun parse-debug (parser input)
+  (funcall parser (parcom:in input)))
+
+(defun emit-debug (object)
+  (common-doc.format:emit-to-string
+   (make-instance 'common-html:html)
+   object))
+
 (defmacro scrawl-tags (&body tags)
   `(alt ,@(mapcar (lambda (x)
-                    `(<* (<$ ,(car x)
-                             (alt ,(cadr x)
-                                  (parcom:string ,(format nil "~s" (car x)))))
-                         (parcom:consume #'whitespace-p)))
-                  tags)))
+                    `(<*> (<* (<$ ',(read-from-string
+                                     (format nil "make-~a"
+                                             (symbol-name (car x))))
+                                  (alt ,(cadr x)
+                                       (parcom:string ,(format nil "~s" (car x)))))
+                              (consume #'whitespace-p))
+                          ,@(append (cddr x) '((sexp-body)))))
+                  tags)
+        (sexp-body)))
+
+(defmacro cswitch (&body body)
+  `(or ,@(mapcar (lambda (c) `(char= c ,c))
+                 body)))
+
+(defun whitespace-p (c)
+  (cswitch #\space #\tab #\newline #\return))
 
 ;;;; parsers ----------------------------------------------------------
 
-(defun read-tags ()
-  (scrawl-tags
-    (:heading (char #\#))
-    (:italics (char #\/))
-    (:bold (char #\*))
-    (:code (char #\~))
-    (:link (char #\@))
-    (:literal (char #\=))
-    (:underline (char #\_))
-    (:strikethrough (char #\+))))
+(defun ok (input)
+  (<$ input (parcom:take 0)))
+
+(defun take-string ()
+  (<*> (ok 'make-text)
+       (*> (peek (anybut +sexp-close+))
+           (take-while (lambda (c)
+                         (not (cswitch #\[ #\])))))))
 
 (defun word ()
-  (parcom:take-while
+  (take-while
    (lambda (c)
      (not (whitespace-p c)))))
 
-(defun key ()
-  (<*> (*> (parcom:peek (char #\:)) (word))
-       (*> (parcom:consume #'whitespace-p)
-           (word))))
+(defun read-title ()
+  (<* (*> (peek (anybut +sexp-close+))
+          (take-while (lambda (c)
+                        (not (cswitch #\[ #\] #\newline)))))
+      (consume #'whitespace-p)))
+
+(defun read-unordered-list ())
+(defun read-ordered-list ())
+
+;; TODO: need to not have the lists be nested
+(defun read-tags ()
+  (scrawl-tags
+    (:section
+     (char #\#)
+     (read-title))
+    (:italic (char #\/))
+    (:bold (char #\*))
+    (:web-link
+     (char #\@)
+     (word))
+    (:code (char #\~))
+    (:code-block
+     (char #\$)
+     (word))
+    ;; (:unordered-list
+    ;;  (char #\-)
+    ;;  (read-unordered-list))
+    ;; (:ordered-list
+    ;;  (char #\=)
+    ;;  (read-ordered-list))
+    (:underline (char #\_))
+    (:strikethrough (char #\+))))
 
 (defun sexp-body ()
   (many (alt (read-sexp)
-             (*> (parcom:peek (anybut +sexp-close+))
-                 (take-while (lambda (c)
-                               (not (or (char= c #\[)
-                                        (char= c #\])))))))))
+             (take-string))))
+
+(defun sexp-meta ()
+  (<*> (read-tags)
+       (sexp-keys)
+       (sexp-body)))
 
 (defun read-sexp ()
   (between
    (char +sexp-open+)
-   (sexp-body)
+   (read-tags)
    (char +sexp-close+)))
 
 ;;;; interface --------------------------------------------------------
